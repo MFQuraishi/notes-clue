fb0f162120365a608a495124422213cdb8b90e0b
Md Zaid Quaraishi <mzq7080@gmail.com>
2025-06-10 00:42:45 +0530

    fix notes addition issue

--- src/utils/handleEditorEdits.tsx
+++ src/utils/handleEditorEdits.tsx
@@ -1,13 +1,14 @@
 import { useNotesListStore } from '@/stateManagement/notesListStates';
 import { NoteIdType } from '@/stateManagement/types/notesListTypes';
 import { JSONContent } from '@tiptap/react';
+import { getUUID } from './generateUUID';
 
 export default function handleEditorEdits(json: JSONContent, noteId?: NoteIdType) {
-	const { addNote, editNote } = useNotesListStore.getState();
+	const { editNote } = useNotesListStore.getState();
 
 	if (noteId) {
 		editNote(noteId, json);
 	} else {
-		addNote(json);
+		editNote(getUUID(), json);
 	}
 }
